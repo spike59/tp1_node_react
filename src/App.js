9cfd85d4a065b0f139fcab0e637e613ed1a25986
adam <adam.kassir@gmail.com>
2022-02-02 10:32:22 +0100

    maj index et app

--- src/App.js
+++ src/App.js
@@ -7,20 +7,40 @@ import { GenderMenu } from './components/gender_menu';
 import { CategoryMenu } from './components/category_menu';
 import {Contact} from './components/contact';
 import {FilterMenu} from './components/filter_menu';
+import {Routes,Route} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <p>bonjour</p>
-          <Nav/>
-          <Header/>
-          <FilterMenu gender_id="1" category_id="1" />
-          {/* <GenderMenu/>
-          <CategoryMenu/> */}
-          <Products/>
-          {/* <Contact/> */}
-          <Footer/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Routes>
     </div>
   );
 }
 
+function HomePage() {
+  return (
+    <>
+      <Nav/>
+      <Header/>
+      <FilterMenu gender_id="1" category_id="1" />
+      {/* <GenderMenu/>
+      <CategoryMenu/> */}
+      <Products/>
+      <Footer/>
+    </>
+  );
+}
+function ContactPage() {
+  return (
+    <>
+      <Nav/>
+      <Header/>
+      <Contact/>
+      <Footer/>
+    </>
+  );
+}
 export default App;
