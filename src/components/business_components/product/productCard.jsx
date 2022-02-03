a88c9f9407aa5c6baa4ce30a17fd727bdcd0f473
adam <adam.kassir@gmail.com>
2022-02-03 16:36:29 +0100

    ajout hommes et gestion image url ou public

--- src/components/business_components/product/productCard.jsx
+++ src/components/business_components/product/productCard.jsx
@@ -2,10 +2,18 @@ import { Link } from 'react-router-dom'
 
 export const ProductCard = (props) => {
     const { id, title, image, price, gender_id, category_id } = props
+    let img_url
+    !image.startsWith("http")?img_url = "assets/images/" + image :img_url = image
+    // if (!image.startsWith("http")){
+    //     img_url = "assets/images/" + image
+    // }else{
+    //     img_url = image
+    // }
+
     return (
         <div className="col mb-5">
             <div className="card h-100">
-                <img className="card-img-top" src={image} alt="..." />
+                <img className="card-img-top" src={img_url} alt="..." />
                 <div className="card-body p-4">
                     <div className="text-center">
                         <h5 className="fw-bolder">{title}</h5>
--- src/components/business_components/product/productFull.jsx
+++ src/components/business_components/product/productFull.jsx
@@ -2,29 +2,32 @@ import { Link } from 'react-router-dom'
 
 export const ProductFull = (props) => {
     const { id, title, description, image, price, gender_id, category_id } = props
+    let img_url
+    !image.startsWith("http") ? img_url = window.location.origin.toString() + "/assets/images/" + image : img_url = image
 
     return (
-        <div className="row m-3">
-            <Link to="/" className="btn btn-outline-dark mt-auto">retour</Link>
-            <div className="col m-3">
-                <div className="card h-100">
-                    <img className="card-img-top" src={image} alt="..." />
-                    <div className="card-body p-4">
-                        <div className="text-center">
-                            <h5 className="fw-bolder">{title}</h5>
-                            {price}€
-                        </div>
-                    </div>
-                    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <p>{gender_id} - {category_id}</p>
-                    </div>
-                    <div>
-                        <p>{description}</p>
-                    </div>
+        <div className="container">
+            <div className="row m-3">
+                <Link to="/" className="btn btn-outline-dark mt-auto">retour</Link>
+                <div className="col-12">
+                    <h1>{title}</h1>
                 </div>
             </div>
+            <div className="row">
+                <div className="col-6">
+                    <img src={img_url} alt="" />
+                </div>
+                <div className="col-6">
+                    <div>{description}</div>
+                    <div>{price}</div>
+                    <a href="#" className="btn btn-outline-dark"> add to cart</a>
+                </div>
+            </div>
+
+            <p>{gender_id} - {category_id}</p>
             <Link to="/" className="btn btn-outline-dark mt-auto">retour</Link>
+
         </div>
     )
-    
+
 }
